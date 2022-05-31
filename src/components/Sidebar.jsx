b8595ef2da77b0ce22d447ec192e3c4a828250d7
Atur-js <atuor127@gmail.com>
2022-05-31 05:40:16 +0430

    toggle Dark/lite mode

--- src/components/Sidebar.jsx
+++ src/components/Sidebar.jsx
@@ -4,3 +4,16 @@ import Toggle from "./Theme/Toggle";
 import { ReactComponent as Settings } from "../assets/settings.svg";
 import { ReactComponent as Campaigns } from "../assets/campaign.svg";
 
+export const Sidebar = () => {
+
+  // sidebar--- add toggle Dark/lite mode, campaigns, settings
+
+  return (
+    <>
+      <div className="flex flex-col  ml-3 dark:border-[#4d4d4d] border-r-2
+        border-slate-300 w-1/4 h-96 float-left h-screen">
+
+        <div className="pl-3 mt-4 md:pl-0 sm:pl-4">
+          <Toggle />
+        </div>
+
