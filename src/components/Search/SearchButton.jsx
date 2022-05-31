1a39c542ab7c8798b787829478b83378118ab3ee
Atur-js <atuor127@gmail.com>
2022-05-31 05:51:24 +0430

    change handler

--- src/components/Search/SearchButton.jsx
+++ src/components/Search/SearchButton.jsx
@@ -6,3 +6,11 @@ import { useNavigate } from "react-router";
 const SearchButton = () => {
   const [text, setText] = useState("");
   const navigate = useNavigate();
+
+  // get event.target.value
+  const getInputValue = (event) => {
+    const userValue = event.target.value;
+    setText(event.target.value);
+    console.log(userValue);
+  };
+
