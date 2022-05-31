c010d03e79a89b00b33df0ec66bda247a9388d33
Atur-js <atuor127@gmail.com>
2022-05-31 05:25:41 +0430

    create theme provider

--- src/components/Theme/ThemeContext.js
+++ src/components/Theme/ThemeContext.js
@@ -18,3 +18,35 @@ const getInitialTheme = () => {
   return "light";
 };
 
+// create theme provider and import in App
+
+export const ThemeContext = createContext();
+
+
+export const ThemeProvider = ({ initialTheme, children }) => {
+  const [theme, setTheme] = useState(getInitialTheme);
+
+  const checkTheme = (existing) => {
+    const root = window.document.documentElement;
+    const isDark = existing === "dark";
+
+    root.classList.remove(isDark ? "light" : "dark");
+    root.classList.add(existing);
+
+    localStorage.setItem("current-theme", existing);
+  };
+
+  if (initialTheme) {
+    checkTheme(initialTheme);
+  }
+
+  React.useEffect(() => {
+    checkTheme(theme);
+  }, [theme]);
+
+  return (
+    <ThemeContext.Provider value={{ theme, setTheme }}>
+      {children}
+    </ThemeContext.Provider>
+  );
+};
