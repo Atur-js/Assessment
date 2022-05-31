2035b55938f82571e8c89612e6dfa0b845ee95c5
Atur-js <atuor127@gmail.com>
2022-05-31 05:24:39 +0430

    initial theme

---
+++ src/components/Theme/ThemeContext.js
@@ -0,0 +1,20 @@
+import React, { useState, createContext } from "react";
+
+
+// create ThemeContext provider and Toggle
+
+// initial theme
+
+const getInitialTheme = () => {
+  if (typeof window !== "undefined" && window.localStorage) {
+    const storedPrefs = window.localStorage.getItem("current-theme");
+    if (typeof storedPrefs === "string") {
+      return storedPrefs;
+    }
+    if (window.matchMedia("(prefers-color-scheme: dark)").matches) {
+      return "dark";
+    }
+  }
+  return "light";
+};
+
