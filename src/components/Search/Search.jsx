bbb6f70e610f524e3409296338fed214c83e2a7e
Atur-js <atuor127@gmail.com>
2022-05-31 05:35:51 +0430

    Search component that result show

---
+++ src/components/Search/Search.jsx
@@ -0,0 +1,13 @@
+import React from 'react';
+import { Campaigns } from '../Campaigns/Campaigns';
+
+// after click search input, this component displaying 
+const Search = () => {
+  return (
+    <>
+      <Campaigns />
+    </>
+  );
+}
+
+export default Search
