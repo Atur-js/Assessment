c0933d30b5baf02a2357d580ad4cd2ee9caaec04
Atur-js <atuor127@gmail.com>
2022-05-31 05:49:06 +0430

    useText and navigate for show result

--- src/components/Search/SearchButton.jsx
+++ src/components/Search/SearchButton.jsx
@@ -3,3 +3,6 @@ import { useNavigate } from "react-router";
 
 // search button and show result
 
+const SearchButton = () => {
+  const [text, setText] = useState("");
+  const navigate = useNavigate();
