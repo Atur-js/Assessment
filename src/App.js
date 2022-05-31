1076e9eb2258706b3737cec6cf1b0149940eb149
Atur-js <atuor127@gmail.com>
2022-05-31 04:53:07 +0430

    import components and react-router-dom

--- src/App.js
+++ src/App.js
@@ -1,5 +1,14 @@
-import logo from './logo.svg';
-import './App.css';
+import "./App.css";
+import { Sidebar } from "./components/Sidebar";
+import {  Route, Routes, Navigate } from "react-router-dom";
+import { Campaigns } from "./components/Campaigns/Campaigns";
+import { Home } from "./components/Campaigns/Home";
+import { Google } from "./components/Campaigns/Google";
+import { Settings } from "./components/Settings";
+import { ThemeProvider } from "./components/Theme/ThemeContext";
+import Hubspot from "./components/Campaigns/Hubspot";
+import Search from "./components/Search/Search";
+import {Pipedrive} from "./components/Campaigns/Pipedrive";
 
 function App() {
   return (
