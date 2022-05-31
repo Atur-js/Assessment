175d2e0a477f8184578168631e266d5f2b06b492
Atur-js <atuor127@gmail.com>
2022-05-31 05:39:34 +0430

    import toggle and Link

--- src/components/Sidebar.jsx
+++ src/components/Sidebar.jsx
@@ -1,3 +1,6 @@
+import React from "react";
+import {Link} from "react-router-dom";
+import Toggle from "./Theme/Toggle";
 import { ReactComponent as Settings } from "../assets/settings.svg";
 import { ReactComponent as Campaigns } from "../assets/campaign.svg";
 
