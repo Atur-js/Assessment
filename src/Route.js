2eef336826aacc004bb13495083895bb1cdf228f
Atur-js <atuor127@gmail.com>
2022-06-01 15:37:36 +0430

    use hooks to create route

---
+++ src/Route.js
@@ -0,0 +1,12 @@
+import React, { useState, useMemo, createContext } from "react";
+
+export const pagesMapping = {
+  home: "home",
+  campaigns: "campaigns",
+  settings: "settings",
+  google: "google",
+  hubspot: "hubspot",
+  pipedrive: "pipedrive",
+  search: "search",
+};
+
