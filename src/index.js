fd9ea993cbe02225ff8d7cd47ab9ade98545beac
Atur-js <atuor127@gmail.com>
2022-06-01 11:36:04 +0430

    remove service worker

--- src/index.js
+++ src/index.js
@@ -2,7 +2,6 @@ import React from 'react';
 import ReactDOM from 'react-dom/client';
 import './index.css';
 import App from './App';
-import * as serviceWorkerRegistration from "./serviceWorkerRegistration";
 import { BrowserRouter } from "react-router-dom";
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
@@ -14,6 +13,6 @@ root.render(
   </React.StrictMode>
 );
 
-serviceWorkerRegistration.register();
+
 
 
